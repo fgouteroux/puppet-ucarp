407173c72cbfd92f149140045bc9c42f7afc7cef
François Gouteroux <francois.gouteroux@d2-si.eu>
2014-08-26 14:36:52 +0200

    Change required file before vip up and function documentation

--- manifests/vip.pp
+++ manifests/vip.pp
@@ -1,3 +1,44 @@
+# === Define Resource Type: ucarp::vip
+#
+# This type will create virtual ip address and a sub-interface, on wich the VIP will be attached to.
+#
+# === Parameters
+#
+# [*ucarp_enabled*]
+# Enable ucarp_mvip service on boot. Values: yes/no.
+#
+# [*ucarp_sub_interface*]
+# Sub-interface name to create.
+#
+# [*ucarp_sub_interface_enabled*]
+# Enabled the VIP after the run. Values: yes/no.
+#
+# [*ucarp_sub_interface_address*]
+# Sub-interface ip address.
+#
+# [*ucarp_sub_interface_netmask*]
+# Sub-interface netmask.
+#
+# [*ucarp_vip*]
+# VIP address
+#
+# [*ucarp_vip_netmask*]
+# VIP netmask
+#
+# [*ucarp_password*]
+# VIP password
+#
+# [*ucarp_options*]
+# VIP options. Type string, Value:"--advskew 1 --advbase 1 --preempt --neutral"
+#
+# Options:
+#  --preempt (-P): becomes a master as soon as possible
+#  --neutral (-n): don't run downscript at start if backup
+#  --advbase=<seconds> (-b <seconds>): advertisement frequency
+#  --advskew=<skew> (-k <skew>): advertisement skew (0-255)
+#
+# For more options "man ucarp"
+#
 define ucarp::vip (
   $ucarp_sub_interface,
   $ucarp_sub_interface_enabled,
@@ -40,7 +81,7 @@ define ucarp::vip (
 
   if $ucarp_sub_interface_enabled == 'yes' {
     exec{"/usr/share/ucarp-mvip/vip-up ${ucarp_sub_interface}":
-      require => File['/usr/share/ucarp-mvip/vip-up'],
+      require => File[$config],
     }->
 
     service { "ucarp-mvip":
